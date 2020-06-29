ce508e6e06c4dacc7ae59121d96be71f92313802
SMun <seonyoung.sonia.mun@gmail.com>
2020-06-29 20:27:58 +1000

    added docs to deploy

---
+++ docs/precache-manifest.e84595f27c80e9a1ce1690118c5e77d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebec4925406943dce66a2a59765693bb",
+    "url": "/awesome-nytimes/index.html"
+  },
+  {
+    "revision": "af55de9c86ccee8f10e2",
+    "url": "/awesome-nytimes/static/css/2.e8e2b19f.chunk.css"
+  },
+  {
+    "revision": "eb540cb48e4a939d4b20",
+    "url": "/awesome-nytimes/static/css/main.07d5f09e.chunk.css"
+  },
+  {
+    "revision": "af55de9c86ccee8f10e2",
+    "url": "/awesome-nytimes/static/js/2.cf1792d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/awesome-nytimes/static/js/2.cf1792d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb540cb48e4a939d4b20",
+    "url": "/awesome-nytimes/static/js/main.c28f9fa2.chunk.js"
+  },
+  {
+    "revision": "163824040f4691fb9a99",
+    "url": "/awesome-nytimes/static/js/runtime-main.970c717e.js"
+  }
+]);
